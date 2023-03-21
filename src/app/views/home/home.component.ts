aba49c6152d1f173b41fbb56282fecf34396a1ad
Samanta Melissa <samanta.melissa13@gmail.com>
2023-03-21 19:35:01 -0300

    Subindo comentarios

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -12,12 +12,14 @@ export class HomeComponent {
 
     constructor(private loginService : LoginService){}
 
+    // Instanciando uma classe:
     userModel = new User()
 
     onSubmit(){
       console.log(this.userModel)
       console.log(this.loginService.login)
     
+      // O subscribe é um operador do Observable.Ao usar ele, estamos falando que, assim que nossa requisição for realizada e nosso texto se transformar em JSON, seremos notificados, e além disso, receberemos as informações do usuários. Esse cara faz tudo de forma rápida e unidimencional. 
       this.loginService.login(this.userModel).subscribe((response) => {
         console.log(response.body.user.email)
       })
